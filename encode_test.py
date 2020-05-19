79387b35e98318f73e7ba6908cbe5d997b5988e5
varun9597 <panditpautrav@gmail.com>
2020-05-20 01:10:46 +0530

    TestCase1

--- encode_test.py
+++ encode_test.py
@@ -5,32 +5,46 @@ import pickle
 import cv2
 import os
 
-encode_file = "encodings.pickle"
+encode_file_path = "encodings/"
+encode_file = "encodings/encodings.pickle"
 
-with open(encode_file,'rb') as rfp:
-    existing_encodes = pickle.load(rfp)
+# with open(encode_file,'rb') as rfp:
+#     existing_encodes = pickle.load(rfp)
 
 print(set(list(existing_encodes["names"])))
 
-# imagePaths = list(paths.list_images("/home/pi/FaceRecogProject/dataset"))
-# #print(imagePaths)
+def check_file_exist():
+    if not os.path.exists(encode_file_path):
+        os.mkdir(path)
+    else:
+        return True
 
-# knownEncodings = []
-# knownNames = []
+def check_encodings_exist():
+    with open(encode_file,'rb') as rfp:
+        existing_encodes = pickle.load(rfp)
 
-# for(i,imagePath) in enumerate(imagePaths):
-#     name = imagePath.split(os.path.sep)[-2]
-#     #print(name)
+names_list = set(list(existing_encodes["names"]))
+encodings_list = set(list(existing_encodes["encodings"]))
 
-#     image = cv2.imread(imagePath)
-#     rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+imagePaths = list(paths.list_images("/home/pi/FaceRecogProject/TestCase1/dataset"))
+print(imagePaths)
 
-#     boxes = face_recognition.face_locations(rgb,model="hog")
+knownEncodings = names_list
+knownNames = encodings_list
 
-#     encodings = face_recognition.face_encodings(rgb, boxes)
+for(i,imagePath) in enumerate(imagePaths):
+    name = imagePath.split(os.path.sep)[-2]
+    print(name)
+
+    image = cv2.imread(imagePath)
+    rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+    boxes = face_recognition.face_locations(rgb,model="hog")
+
+    encodings = face_recognition.face_encodings(rgb, boxes)
 
 
-#     for encoding in encodings:
-#         knownEncodings.append(encoding)
-#         knownNames.append(name)
+    for encoding in encodings:
+        knownEncodings.append(encoding)
+        knownNames.append(name)
     
